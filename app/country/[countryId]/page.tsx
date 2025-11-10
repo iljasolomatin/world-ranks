5da35238438d6c82d32c8864ed8a5e6c898750a2
Ilja Solomatin <ilja.solomatin@bath.edu>
2025-11-10 16:52:14 +0000

    Created main container for individual country page
    Currently displays flag and country name/official name. Still need to correctly style remaining data.

--- app/country/[countryId]/page.tsx
+++ app/country/[countryId]/page.tsx
@@ -1,4 +1,5 @@
 import { getCountriesByCodes } from "@/lib/countries";
+import Image from "next/image";
 
 interface Params {
   params: { countryId: string };
@@ -23,11 +24,25 @@ async function page({ params }: Params) {
   // FETCH WITH .ALL TO GET FLAGS OF BORDER COUNTRIES
   console.log(country);
   return (
-    <div>
-      <h1>{name.common}</h1>
-      <p>{name.official}</p>
-      <p>{population}</p>
-      <p>{area}</p>
+    <div className="bg-muted relative z-20 mx-auto -mt-28 mb-8 flex max-w-2xl flex-col gap-6 rounded-lg px-4 py-6 shadow-lg md:-mt-14 md:px-6">
+      <div className="-mt-16">
+        <Image
+          src={flags.svg}
+          alt={`Flag of ${name.common}`}
+          // 50x38 ratio
+          width={250}
+          height={189}
+          className="mx-auto rounded-lg object-cover"
+        />
+      </div>
+      <div>
+        <h1 className="text-center text-3xl font-semibold">{name.common}</h1>
+        <p className="text-center">{name.official}</p>
+      </div>
+      <div className="flex items-center justify-around">
+        <p>Population | {population}</p>
+        <p>Area | {area}</p>
+      </div>
       <p>{capital}</p>
       <p>{subregion}</p>
       {/* <p>
